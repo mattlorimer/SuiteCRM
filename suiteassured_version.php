241214d3174e710439dcbad24f1a086992f610cb
Matt Lorimer <matthew.lorimer@salesagility.com>
2018-02-02 17:32:54 +0000

    SuiteAssured 1.0.5

--- suiteassured_version.php
+++ suiteassured_version.php
@@ -4,5 +4,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suiteassured_version      = '1.0.0';
-$suiteassured_timestamp    = '2017-05-29 17:00';
+$suiteassured_version      = '1.0.5';
+$suiteassured_timestamp    = '2018-01-29 17:00';
