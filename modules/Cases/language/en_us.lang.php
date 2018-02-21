b4c55b3d15e3e2e0d0a5a67546e1f6416d56e809
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-02-21 16:53:01 +0000

    remove duplicated lang texts

--- modules/Cases/language/en_us.lang.php
+++ modules/Cases/language/en_us.lang.php
@@ -128,7 +128,6 @@ $mod_strings = array(
     'LBL_CONTACT_CREATED_BY_NAME' => 'Created by contact',
     'LBL_CONTACT_CREATED_BY' => 'Created by',
     'LBL_CASE_UPDATE_FORM' => 'Updates - Attachment form', //Form for attachments on case updates
-    'LBL_CASE_UPDATE_FORM' => 'Update attachment form',
     'LBL_SUGGESTION' => 'Suggestion',
 );
 
